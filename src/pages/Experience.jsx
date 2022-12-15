f3258671cdfa2d686c24590b99fe86fa718d8309
Varun <varunmukherjee7@gmail.com>
2022-12-15 14:00:16 +0530

    base

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import styled from 'styled-components'
 
 function Experience() {
   return (
-    <div>Experience</div>
+    <MainContainer>
+      <Container>
+
+      </Container>
+    </MainContainer>
   )
 }
+
+const MainContainer = styled.div`
+  height: 100vh;
+  width: 100vw;
+`
+
+const Container = styled.div`
+  background-color: ${props => `rgba(${props.theme.bodyRgba},0.8)`};
+  width: 100%;
+  
+`
 
 export default Experience
